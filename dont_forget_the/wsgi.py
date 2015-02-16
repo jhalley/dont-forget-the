929ca0187f382dc4eee381e68f3ec950d6e6acd7
Jhalley Badua de Castro <joseph.halley@gmail.com>
2015-02-16 08:12:24 +0000

    Initial commit

---
+++ dont_forget_the/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for dont_forget_the project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dont_forget_the.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
