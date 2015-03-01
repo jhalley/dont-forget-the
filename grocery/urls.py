83781a66423a7a72cdb41b0e365c200c4fb2030d
Jhalley Badua de Castro <joseph.halley@gmail.com>
2015-03-01 17:09:23 +0000

    Init commit for api component

--- grocery/urls.py
+++ grocery/urls.py
@@ -3,4 +3,7 @@ from grocery import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    
+    # api stuff
+    url(r'^api/list/$', views.list, name='list'),
 )
--- grocery/views.py
+++ grocery/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+import json
 
 from grocery.models import *
 
 # Create your views here.
 def index(request):
     context = {
-        'lists': [str(i) for i in List.objects.all()]
+        'lists': [i for i in List.objects.all()]
     }
     return render(request, 'grocery/index.html', context)
+    
+# API
+def list(request):
+    response_data = []
+    for i in List.objects.all():
+        response_data.append({
+            'id': i.id,
+            'title': i.title,
+            'status': i.status
+        })
+    
+    return HttpResponse(json.dumps({
+        'data': response_data
+    }), content_type="application/json")
