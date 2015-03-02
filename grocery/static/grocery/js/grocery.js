81d45311ae74324dda105fad24248cf8aca43821
Jhalley Badua de Castro <joseph.halley@gmail.com>
2015-03-02 18:00:56 +0000

    Viewing of list items

--- grocery/static/grocery/js/grocery.js
+++ grocery/static/grocery/js/grocery.js
@@ -6,16 +6,22 @@ angular.module('groceryApp', ['ngTouch'])
     // MAIN VARIABLES 
     var d = new Date();
     $scope.new_list_name = 'Grocery List ' + d.getFullYear() + '-' + ('0' + (d.getMonth()+1)).slice(-2) + '-' + ('0' + d.getDate()).slice(-2);
-    $scope.list = []
+    $scope.list = [];
+    $scope.list_items = [];
     $scope.view_mode = 'lists'; // lists, List_items
     
-    // INIT
-    $scope.get_list = function(){
-      $http.get('/grocery/api/list').
+    // Functions
+    $scope.view_list_items = function(l){
+      $scope.get_list_items(l);
+      $scope.view_mode = 'list_items';
+    }
+    
+    $scope.get_list_items = function(l){
+      $http.get('/grocery/api/list/' + l.id).
        success(function(data, status, headers, config) {
          // this callback will be called asynchronously
          // when the response is available
-         $scope.list = data['data'];
+         $scope.list_items = data['data'];
        }).
        error(function(data, status, headers, config) {
          // called asynchronously if an error occurs
@@ -23,12 +29,6 @@ angular.module('groceryApp', ['ngTouch'])
          alert('Error accessing site. Please refresh page.')
        });
     };
-    $scope.get_list();
-      
-    // Functions
-    $scope.view_list_items = function(l){
-      $scope.view_mode = 'list_items';
-    }
     
     $scope.add_new_list = function(){
       $http.post('/grocery/api/list/', {
@@ -48,9 +48,23 @@ angular.module('groceryApp', ['ngTouch'])
           alert('Error creating new list.')
         });
     }
+    
+    $scope.get_list = function(){
+      $http.get('/grocery/api/list').
+       success(function(data, status, headers, config) {
+         // this callback will be called asynchronously
+         // when the response is available
+         $scope.list = data['data'];
+       }).
+       error(function(data, status, headers, config) {
+         // called asynchronously if an error occurs
+         // or server returns response with an error status.
+         alert('Error accessing site. Please refresh page.')
+       });
+    };
       
-    //$scope.todos = [
-    //  {text:'learn angular', done:true},
-    //  {text:'build an angular app', done:false}];
+    // INIT
+    $scope.get_list();
+      
       
   }]);
