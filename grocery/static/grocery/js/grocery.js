838f579a0c0542cc9d2132db7a44aea888bd2607
Jhalley Badua de Castro <joseph.halley@gmail.com>
2015-03-02 15:59:01 +0000

    Add new list

--- grocery/static/grocery/js/grocery.js
+++ grocery/static/grocery/js/grocery.js
@@ -1,22 +1,48 @@
-angular.module('groceryApp', [])
+angular.module('groceryApp', ['ngTouch'])
   .controller('GroceryController', ['$scope', '$http', function($scope, $http) {
     // FOR DEBUGGING
     window.scope = $scope;
     
     // MAIN VARIABLES 
+    var d = new Date();
+    $scope.new_list_name = 'Grocery List ' + d.getFullYear() + '-' + ('0' + (d.getMonth()+1)).slice(-2) + '-' + ('0' + d.getDate()).slice(-2);
     $scope.list = []
     
     // INIT
-    $http.get('/grocery/api/list').
-      success(function(data, status, headers, config) {
-        // this callback will be called asynchronously
-        // when the response is available
-        $scope.list = data['data'];
-      }).
-      error(function(data, status, headers, config) {
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
-      });
+    $scope.get_list = function(){
+      $http.get('/grocery/api/list').
+       success(function(data, status, headers, config) {
+         // this callback will be called asynchronously
+         // when the response is available
+         $scope.list = data['data'];
+       }).
+       error(function(data, status, headers, config) {
+         // called asynchronously if an error occurs
+         // or server returns response with an error status.
+         alert('Error accessing site. Please refresh page.')
+       });
+    };
+    $scope.get_list();
+      
+    // Functions
+    $scope.add_new_list = function(){
+      $http.post('/grocery/api/list/', {
+          name: $scope.new_list_name,
+        }, {
+          headers: {'X-CSRFToken': $.cookie('csrftoken')},
+        }).
+        success(function(data, status, headers, config) {
+          // this callback will be called asynchronously
+          // when the response is available
+          $scope.get_list();
+          $('#addNewListModal').modal('toggle');
+        }).
+        error(function(data, status, headers, config) {
+          // called asynchronously if an error occurs
+          // or server returns response with an error status.
+          alert('Error creating new list.')
+        });
+    }
       
     //$scope.todos = [
     //  {text:'learn angular', done:true},
--- grocery/views.py
+++ grocery/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from django.views.decorators.csrf import csrf_exempt    # Because it's a pain to do this with mobile
 import json
 
 from grocery.models import *
@@ -13,15 +14,31 @@ def index(request):
     return render(request, 'grocery/index.html', context)
     
 # API
+@csrf_exempt
 def list(request):
-    response_data = []
-    for i in List.objects.all():
-        response_data.append({
-            'id': i.id,
-            'title': i.title,
-            'status': i.status
-        })
+    return_data = {
+        'data': [],
+        'len': 0,
+        'status': 'OK',
+        'status_message': 'OK',
+    }
     
-    return HttpResponse(json.dumps({
-        'data': response_data
-    }), content_type="application/json")
+    if request.method == 'GET':
+        for i in List.objects.all():
+            return_data['data'].append({
+                'id': i.id,
+                'title': i.title,
+                'status': i.status
+            })
+        return_data['len'] = len(return_data['data'])
+        
+        return HttpResponse(json.dumps(return_data), content_type="application/json")
+    elif request.method == 'POST':
+        l = List(title=json.loads(request.body)['name'], status=List.OPEN)
+        l.save()
+        #l = List(title = 
+        return HttpResponse(json.dumps(return_data))
+    else:
+        return_data['status'] = 'ERROR'
+        return_data['status_message'] = 'Request method not supported'
+        return HttpResponse(json.dumps(return_data))
